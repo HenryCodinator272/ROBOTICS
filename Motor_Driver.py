b6ad2bac327edab289dc9945dc7630e4f0a26c80
Henry Stern <26hstern@hra.org>
2026-08-16 19:31:27 -0400

    new

--- Motor_Driver.py
+++ Motor_Driver.py
@@ -3,80 +3,79 @@ try:
 except ImportError:
     import SimulRPi.GPIO as GPIO
 import time
-import numpy as np
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(29, GPIO.OUT)
 
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(31, GPIO.OUT)
 
-motor1Speed = GPIO.PWM(17, 1000) #1000 is the frequency
-motor2Speed = GPIO.PWM(19, 1000)
+motor1Speed = GPIO.PWM(29, 1000) #1000 is the frequency
+motor2Speed = GPIO.PWM(31, 1000)
 motor1Speed.start(0)
 motor2Speed.start(0)
 
 def forward(speed):
     print(f'Going forward at {speed} speed')
-    GPIO.output(16, True)
-    GPIO.output(18, True)
+    GPIO.output(11, True)
+    GPIO.output(13, True)
 
     motor1Speed.ChangeDutyCycle(speed)
     motor2Speed.ChangeDutyCycle(speed)
 
 def backward(speed):
     print(f'Going backward at {speed} speed')
-    GPIO.output(16, False)
-    GPIO.output(18, False)
+    GPIO.output(11, False)
+    GPIO.output(13, False)
 
     motor1Speed.ChangeDutyCycle(speed)
     motor2Speed.ChangeDutyCycle(speed)
 
-def left_forward(speed, r = 0, diff = 160): #diff --> distance_between_wheels ... r --> radius
-    GPIO.output(16, True)
-    GPIO.output(18, True)
+def left_forward(speed, r = 0, diff = 144): #diff --> distance_between_wheels ... r --> radius
+    GPIO.output(11, True)
+    GPIO.output(13, True)
 
-    inner_wheel_speed = r * speed / (r + diff / 2)
-    outer_wheel_speed = (r+diff)*speed/(r+diff/2)
-
-    motor1Speed.ChangeDutyCycle(inner_wheel_speed)
-    motor2Speed.ChangeDutyCycle(outer_wheel_speed)
-
-def left_backward(speed, r = 0, diff = 160):
-    GPIO.output(16, False)
-    GPIO.output(18, False)
-
-    inner_wheel_speed = r * speed / (r + diff / 2)
-    outer_wheel_speed = (r + diff) * speed / (r + diff / 2)
+    inner_wheel_speed = r * speed / (r + diff)
+    outer_wheel_speed = speed
 
     motor1Speed.ChangeDutyCycle(inner_wheel_speed)
     motor2Speed.ChangeDutyCycle(outer_wheel_speed)
 
-def right_forward(speed, r=0, diff=160):  # diff --> distance_between_wheels ... r --> radius
-    GPIO.output(16, True)
-    GPIO.output(18, True)
+def left_backward(speed, r = 0, diff = 144):
+    GPIO.output(11, False)
+    GPIO.output(13, False)
 
-    inner_wheel_speed = r * speed / (r + diff / 2)
-    outer_wheel_speed = (r + diff) * speed / (r + diff / 2)
+    inner_wheel_speed = r * speed / (r + diff)
+    outer_wheel_speed = speed
+
+    motor1Speed.ChangeDutyCycle(inner_wheel_speed)
+    motor2Speed.ChangeDutyCycle(outer_wheel_speed)
+
+def right_forward(speed, r=0, diff=144):  # diff --> distance_between_wheels ... r --> radius
+    GPIO.output(11, True)
+    GPIO.output(13, True)
+
+    inner_wheel_speed = r * speed / (r + diff)
+    outer_wheel_speed = speed
 
     motor1Speed.ChangeDutyCycle(outer_wheel_speed)
     motor2Speed.ChangeDutyCycle(inner_wheel_speed)
 
-def right_backward(speed, r=0, diff=160):
-    GPIO.output(16, False)
-    GPIO.output(18, False)
+def right_backward(speed, r=0, diff=144):
+    GPIO.output(11, False)
+    GPIO.output(13, False)
 
-    inner_wheel_speed = r * speed / (r + diff / 2)
-    outer_wheel_speed = (r + diff) * speed / (r + diff / 2)
+    inner_wheel_speed = r * speed / (r + diff)
+    outer_wheel_speed = speed
 
     motor1Speed.ChangeDutyCycle(outer_wheel_speed)
     motor2Speed.ChangeDutyCycle(inner_wheel_speed)
 
 def stop():
-    GPIO.output(16, False)
-    GPIO.output(18, False)
+    GPIO.output(11, False)
+    GPIO.output(13, False)
 
     motor1Speed.ChangeDutyCycle(0)
     motor2Speed.ChangeDutyCycle(0)
